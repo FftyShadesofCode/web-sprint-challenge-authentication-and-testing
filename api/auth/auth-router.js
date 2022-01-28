915e0ee3ad57c0e745b0b22e9470b8e61436de5d
FiftyShadesofCode <philiprmcdavid@gmail.com>
2022-01-28 13:05:07 -0500

    adding files and folders necessary

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,59 +1,93 @@
 const router = require('express').Router();
+const {tokenBuilder} = require('./auth-helpers')
+const bcrypt = require('bcryptjs')
+const User = require('./auth-model')
+const {errorHandling, checkIfUsernameTaken, checkIfUsernameExists, validation,} = require('../middleware/restricted')
 
-router.post('/register', (req, res) => {
-  res.end('implement register, please!');
-  /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
-    DO NOT EXCEED 2^8 ROUNDS OF HASHING!
+const {BCRYPT_ROUNDS} = require('../../data/dbConfig')
 
-    1- In order to register a new account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel", // must not exist already in the `users` table
-        "password": "foobar"          // needs to be hashed before it's saved
-      }
+router.post('/register', validation, checkIfUsernameTaken, (req, res, next) => {
+    let user = req.body
 
-    2- On SUCCESSFUL registration,
-      the response body should have `id`, `username` and `password`:
-      {
-        "id": 1,
-        "username": "Captain Marvel",
-        "password": "2a$08$jG.wIGR2S4hxuyWNcBf9MuoC4y0dNy7qC/LbmtuFBSdIhWks2LhpG"
-      }
+    const hash = bcrypt.hashSync(user.password, BCRYPT_ROUNDS)
 
-    3- On FAILED registration due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
+    user.password = hash
 
-    4- On FAILED registration due to the `username` being taken,
-      the response body should include a string exactly as follows: "username taken".
-  */
+    User.create(user)
+        .then(savedUser => {
+            console.log(savedUser)
+            res.status(201).json(savedUser)
+        })
+        .catch(next)
+    /*
+      IMPLEMENT
+      You are welcome to build additional middlewares to help with the endpoint's functionality.
+      DO NOT EXCEED 2^8 ROUNDS OF HASHING!
+
+      1- In order to register a new account the client must provide `username` and `password`:
+        {
+          "username": "Captain Marvel", // must not exist already in the `users` table
+          "password": "foobar"          // needs to be hashed before it's saved
+        }
+
+      2- On SUCCESSFUL registration,
+        the response body should have `id`, `username` and `password`:
+        {
+          "id": 1,
+          "username": "Captain Marvel",
+          "password": "2a$08$jG.wIGR2S4hxuyWNcBf9MuoC4y0dNy7qC/LbmtuFBSdIhWks2LhpG"
+        }
+
+      3- On FAILED registration due to `username` or `password` missing from the request body,
+        the response body should include a string exactly as follows: "username and password required".
+
+      4- On FAILED registration due to the `username` being taken,
+        the response body should include a string exactly as follows: "username taken".
+    */
 });
 
-router.post('/login', (req, res) => {
-  res.end('implement login, please!');
-  /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
+router.post('/login', validation, checkIfUsernameExists, (req, res, next) => {
+    let {username, password} = req.body
 
-    1- In order to log into an existing account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel",
-        "password": "foobar"
-      }
+    User.findBy({username})
+        .then(([user]) => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                const token = tokenBuilder(user)
+                res.status(200).json({
+                    message: `welcome, ${user.username}`,
+                    token,
+                })
+            } else {
+                next({status: 401, message: 'invalid credentials'})
+            }
+        })
+        .catch(next)
 
-    2- On SUCCESSFUL login,
-      the response body should have `message` and `token`:
-      {
-        "message": "welcome, Captain Marvel",
-        "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
-      }
+    /*
+      IMPLEMENT
+      You are welcome to build additional middlewares to help with the endpoint's functionality.
 
-    3- On FAILED login due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
+      1- In order to log into an existing account the client must provide `username` and `password`:
+        {
+          "username": "Captain Marvel",
+          "password": "foobar"
+        }
 
-    4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
-      the response body should include a string exactly as follows: "invalid credentials".
-  */
+      2- On SUCCESSFUL login,
+        the response body should have `message` and `token`:
+        {
+          "message": "welcome, Captain Marvel",
+          "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
+        }
+
+      3- On FAILED login due to `username` or `password` missing from the request body,
+        the response body should include a string exactly as follows: "username and password required".
+
+      4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
+        the response body should include a string exactly as follows: "invalid credentials".
+    */
 });
+
+router.use(errorHandling)
 
 module.exports = router;
--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,55 @@
 // Write your tests here
+const server = require('./server')
+const request = require('supertest')
+const db = require('../data/dbConfig')
+
 test('sanity', () => {
-  expect(true).toBe(false)
+    expect(true).toBe(true)
+})
+
+beforeAll(async () => {
+    await db.migrate.rollback()
+    await db.migrate.latest()
+})
+
+afterAll(async () => {
+    await db.destroy()
+})
+
+test('correct environment', () => {
+    expect(process.env.NODE_ENV).toBe('testing')
+})
+
+describe('Auth Router', () => {
+    describe('POST /api/auth/register', () => {
+        let res
+        beforeEach(async () => {
+            res = await request(server)
+                .post('/api/auth/register')
+                .send({username: 'Frog', password: 'Ribbit'})
+        })
+        test('responds with 201 created', async () => {
+            expect(res.status).toBe(201)
+        })
+        test('new user is sent back', async () => {
+            expect(res.body).toMatchObject({message: 'username taken'})
+        })
+    })
+
+    describe('Post /api/auth/login', () => {
+        test('logs in', async () => {
+            const res = await request(server)
+                .post('/api/auth/login')
+                .send({username: 'Frog', password: 'Ribbit'})
+
+            expect(res.body.message).toBe('welcome, Frog')
+        })
+        test('needs valid credentials', async () => {
+            const res = await request(server)
+                .post('/api/auth/login')
+                .send()
+
+            expect(res.body.message).toBe('username and password required')
+        })
+    })
 })
